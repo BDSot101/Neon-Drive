0e2a647d025aaccc352b514f22244f058cfaa5c8
BDSot101 <beaglesloveme2@gmail.com>
2017-02-06 10:55:08 -0700

    fixing resources

--- Blaster Master/Properties/Resources.Designer.cs
+++ Blaster Master/Properties/Resources.Designer.cs
@@ -353,6 +353,16 @@ namespace BlasterMaster.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap pickupC2 {
+            get {
+                object obj = ResourceManager.GetObject("pickupC2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap playerA {
             get {
                 object obj = ResourceManager.GetObject("playerA", resourceCulture);
