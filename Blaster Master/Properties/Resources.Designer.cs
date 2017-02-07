df9fba199ed4a299cd18000b4547f6a11008c008
BDSot101 <beaglesloveme2@gmail.com>
2017-02-07 11:16:18 -0700

    back to future

--- Blaster Master/Properties/Resources.Designer.cs
+++ Blaster Master/Properties/Resources.Designer.cs
@@ -383,6 +383,16 @@ namespace BlasterMaster.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap playerBack2Fut {
+            get {
+                object obj = ResourceManager.GetObject("playerBack2Fut", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap playerBullet {
             get {
                 object obj = ResourceManager.GetObject("playerBullet", resourceCulture);
