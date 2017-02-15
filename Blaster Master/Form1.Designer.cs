aaa2ff9dbe7e7126e112f2f57c89415f397b8cfe
BDSot101 <beaglesloveme2@gmail.com>
2017-02-15 11:40:55 -0700

    changed form text

--- Blaster Master/Form1.Designer.cs
+++ Blaster Master/Form1.Designer.cs
@@ -35,13 +35,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(597, 771);
+            this.ClientSize = new System.Drawing.Size(589, 767);
             this.DoubleBuffered = true;
             this.MaximumSize = new System.Drawing.Size(605, 805);
             this.MinimumSize = new System.Drawing.Size(605, 805);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Blaster Master (World Edition C#)";
+            this.Text = "Neon Drive (World Edition C#)";
             this.ResumeLayout(false);
 
         }
