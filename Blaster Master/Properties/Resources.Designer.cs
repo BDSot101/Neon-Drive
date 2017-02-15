23b013054ae2eff6c61ffda9da7b5a441083e43e
BDSot101 <beaglesloveme2@gmail.com>
2017-02-15 11:42:57 -0700

    added car ico

--- Blaster Master/Properties/Resources.Designer.cs
+++ Blaster Master/Properties/Resources.Designer.cs
@@ -121,6 +121,16 @@ namespace BlasterMaster.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon car_23_256 {
+            get {
+                object obj = ResourceManager.GetObject("car_23_256", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap enemyA1 {
